62f2af9287e45668f3810de4da47bccea120654d
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2023-12-29 02:00:40 +0530

    Designing Part Started

--- BudgetBuddy/Controllers/CategoryController.cs
+++ BudgetBuddy/Controllers/CategoryController.cs
@@ -47,7 +47,7 @@ namespace BudgetBuddy.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
